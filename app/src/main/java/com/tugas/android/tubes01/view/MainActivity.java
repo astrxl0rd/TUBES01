5f9b0e58776b9357ff92765cef0d87027f88c13d
Reynaldi Rahadian <astrxl0rd@users.noreply.github.com>
2020-10-22 17:50:55 +0700

    Initial commit

---
+++ app/src/main/java/com/tugas/android/tubes01/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tugas.android.tubes01.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.tugas.android.tubes01.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
